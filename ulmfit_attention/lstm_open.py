352e6254ed64caa3aa1aef4b7a01972c03a4b642
Tomasz Pietruszka <tomek.pietruszka@gmail.com>
2019-11-29 17:04:47 +0100

    LSTMOpen - added an option to return memory contents

--- ulmfit_attention/lstm_open.py
+++ ulmfit_attention/lstm_open.py
@@ -6,7 +6,8 @@ import torch
 
 class LSTMOpen(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers: int = 1, bias: bool = True,
-                 batch_first: bool = False, dropout: float = 0., bidirectional: bool = False):
+                 batch_first: bool = False, dropout: float = 0., bidirectional: bool = False,
+                 return_memory: bool = False):
         if bidirectional:
             raise NotImplementedError("Not supported yet")  # TODO: implement bidirectional
         if not batch_first:
@@ -16,6 +17,7 @@ class LSTMOpen(nn.Module):
         if dropout != 0.:
             raise NotImplementedError("Not supported yet")  # TODO: implement dropout as in LSTM
         super(LSTMOpen, self).__init__()
+        self.return_memory = return_memory
         self.cell = nn.LSTMCell(input_size, hidden_size, bias)
         # enable LSTM-like access to parameters, so that load_state_dict() will work (with strict=False)
         self.weight_ih_l0 = self.cell.weight_ih
@@ -24,6 +26,7 @@ class LSTMOpen(nn.Module):
         self.bias_hh_l0 = self.cell.bias_hh
 
         # TODO: add option to log all memory states
+        # TODO: solve loading/conversion from LSTM in a better way
 
     def forward(self, inp: Tensor, hidden: Tuple[Tensor, Tensor]) -> Tuple[Tensor, Tuple[Tensor, Tensor]]:
         bs, sl, di = inp.shape
@@ -31,12 +34,14 @@ class LSTMOpen(nn.Module):
         outputs = []
         for i in range(sl):
             hx, cx = self.cell(inp[:, i, :], (hx, cx))
-            outputs.append(hx)
+            if self.return_memory:
+                outputs.append(cx)
+            else:
+                outputs.append(hx)
         results = torch.stack(outputs).transpose(0, 1)
+        if self.return_memory:
+            results.tanh_()
         return results, (hx.unsqueeze(0), cx.unsqueeze(0))
-
-    def reset(self):
-        return self.cell.reset()
 
     def load_params_from_lstm(self, orig: nn.LSTM):
         self.cell.weight_ih = orig.weight_ih_l0
