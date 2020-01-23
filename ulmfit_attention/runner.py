9e02c372751321fcbfb9978c83857bd3133c8cf0
Tomasz Pietruszka <tomek.pietruszka@gmail.com>
2020-01-23 13:44:42 +0000

    Proper CLI for the runner script

--- ulmfit_attention/runner.py
+++ ulmfit_attention/runner.py
@@ -1,20 +1,13 @@
-import os
 from pathlib import Path
 import traceback
 import time
+import argparse
 from sacred import observers, Experiment
 from ulmfit_attention.task_queue import TaskQueue, Task
 from ulmfit_attention import scenarios  # TODO: import from cwd, not relative to this source
 
-# TODO: get this from command line or sth
-db_name = 'ulmfit_attention'  # TODO: maybe set default to folder name?
-mongo_uri = 'localhost:27017'
-tasks_dir = Path(os.path.dirname(os.path.realpath(os.getcwd()))) / 'tasks'
 
-sleep_time = 30
-
-
-def run_from_queue():
+def run_from_queue(tasks_dir: Path, db_name: str, mongo_uri: str, sleep_time: int):
     observer = observers.MongoObserver.create(mongo_uri, db_name=db_name)
     q = TaskQueue(mongo_uri, db_name, tasks_dir)
     while True:
@@ -48,5 +41,19 @@ def run_task(task: Task, observer: observers.RunObserver):
     return run_res
 
 
+def main():
+    desc = "Run experiments from a MongoDB-based queue. \nShould be ran from a folder containing " \
+           "a `scenarios` module, defining the possible scenarios to run. Concrete tasks - " \
+           "sets of parameters for scenarios - should be defined in json files, in the `tasks_dir` folder"
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('tasks_dir', help='Path to a folder storing task json files, absolute or relative',
+                      type=lambda s: Path(s).resolve())
+    parser.add_argument('db_name', help='MongoDB database name')
+    parser.add_argument('--mongo-uri', help='URI of the MongoDB server instance', default='localhost:27017')
+    parser.add_argument('--sleep-time', type=int, default=30)
+    args = parser.parse_args()
+    run_from_queue(args.tasks_dir, args.db_name, args.mongo_uri, args.sleep_time)
+
+
 if __name__ == "__main__":
-    run_from_queue()
+    main()
