d26c6092c075eef7a86f41a51fe152d341c3e25b
Tomasz Pietruszka <tomek.pietruszka@gmail.com>
2020-03-13 20:13:27 +0000

    fix: updated dependencies when installing as library

--- setup.py
+++ setup.py
@@ -6,9 +6,11 @@ setup(name="ulmfit_attention",
       author_email="tomek.pietruszka@gmail.com",
       description="Modules implementing improved pooling for ULMFiT text classification",
       packages=find_packages(),
-      python_requires='>=3.6',
+      python_requires='>=3.7',
       install_requires=[
           'fastai>=1.0.59',
+          'pytorch>=1.3.1',
+          'hyperspace_explorer>=0.2.0',
       ],
       tests_require=[
             'pytest',
