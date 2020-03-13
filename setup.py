5ca3347cf330f91949e43afbd8927f47ce68f2fd
Tomasz Pietruszka <tomek.pietruszka@gmail.com>
2020-03-13 20:14:11 +0000

    style: consistent formatting in setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
 
-setup(name="ulmfit_attention",
-      version="0.0.1",
-      author="Tomasz Pietruszka",
-      author_email="tomek.pietruszka@gmail.com",
-      description="Modules implementing improved pooling for ULMFiT text classification",
-      packages=find_packages(),
-      python_requires='>=3.7',
-      install_requires=[
-          'fastai>=1.0.59',
-          'pytorch>=1.3.1',
-          'hyperspace_explorer>=0.2.0',
-      ],
-      tests_require=[
-            'pytest',
-      ],
-      )
+setup(
+    name="ulmfit_attention",
+    version="0.0.1",
+    author="Tomasz Pietruszka",
+    author_email="tomek.pietruszka@gmail.com",
+    description="Modules implementing improved pooling for ULMFiT text classification",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        "fastai>=1.0.59",
+        "pytorch>=1.3.1",
+        "hyperspace_explorer>=0.2.0",
+    ],
+    tests_require=["pytest",],
+)
